37ec5a4a5463211c75e9d884fda3041e65b9503f
oliveszn <ekpealamicheal@gmail.com>
2025-07-31 14:03:57 +0100

    did gateway

--- auth-service/src/controllers/authController.js
+++ auth-service/src/controllers/authController.js
@@ -1,8 +1,9 @@
 const logger = require("../utils/logger");
-const { validateRegistration } = require("../utils/Validation");
+const { validateRegistration, validatelogin } = require("../utils/Validation");
 const User = require("../models/User");
 const generateTokens = require("../utils/generateToken");
 const RefreshToken = require("../models/RefreshToken");
+const argon2 = require("argon2");
 
 const resgiterUser = async (req, res) => {
   logger.info("Registration endpoint hit...");
@@ -27,7 +28,9 @@ const resgiterUser = async (req, res) => {
       });
     }
 
-    user = new User({ username, email, password });
+    const hash = await argon2.hash(password);
+
+    user = new User({ username, email, password: hash });
     await user.save();
     logger.warn("User saved successfully", user._id);
 
@@ -48,4 +51,53 @@ const resgiterUser = async (req, res) => {
   }
 };
 
-module.exports = { resgiterUser };
+const loginUser = async (req, res) => {
+  logger.info("Login endpoint hit...");
+  try {
+    const { error } = validatelogin(req.body);
+    if (error) {
+      logger.warn("Validation error", error.details[0].message);
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      logger.warn("Invalid user");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    // user valid password or not
+    // const isValidPassword = await user.comparePassword(password);
+    const checkPasswordMatch = await argon2.verify(user.password, password);
+    if (!checkPasswordMatch) {
+      logger.warn("Invalid password");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user);
+
+    res.json({
+      accessToken,
+      refreshToken,
+      userId: user._id,
+    });
+  } catch (e) {
+    logger.error("Login error occured", e);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { resgiterUser, loginUser };
--- auth-service/src/routes/authRoutes.js
+++ auth-service/src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {
   resgiterUser,
-  //   loginUser,
+  loginUser,
   //   refreshTokenUser,
   //   logoutUser,
 } = require("../controllers/authController");
@@ -9,7 +9,7 @@ const {
 const router = express.Router();
 
 router.post("/register", resgiterUser);
-// router.post("/login", loginUser);
+router.post("/login", loginUser);
 // router.post("/refresh-token", refreshTokenUser);
 // router.post("/logout", logoutUser);
 
